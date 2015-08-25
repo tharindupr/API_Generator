4a6f96af5da86c409707d18e630955a0b4b7ea7d
Milindu Sanoj Kumarage <agentmilindu@gmail.com>
2015-08-25 11:22:20 +0530

    Bear controller added

---
+++ controllers/bear.js
@@ -0,0 +1,10 @@
+var Bear = require('../models/bear');
+
+exports.save = function (req, res) {
+  var bear = new Bear();      // create a new instance of the Bear model
+  bear.name = req.body.name;
+  bear.save(function(err) {
+   if (err) res.send(err);
+   res.json({ message: 'Bear created!' });
+  });
+};
