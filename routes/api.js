be9a8cfb008e1598b99d7d7226de977d335ce949
Milindu Sanoj Kumarage <agentmilindu@gmail.com>
2015-08-25 11:26:37 +0530

    linked the bearController
    This is not the best way, but would be enough for you to get things started. Refactor into correct patterns later :)

--- routes/api.js
+++ routes/api.js
@@ -10,22 +10,8 @@ var router = express.Router();              // get an instance of the express Ro
 router.route('/bears')
 
     // create a bear (accessed at POST http://localhost:8080/api/bears)
-    .post(function(req, res) {
-        
-        var bear = new Bear();      // create a new instance of the Bear model
-        bear.name = req.body.name;  // set the bears name (comes from the request)
-
-        // save the bear and check for errors
-         bear.save(function(err) {
-
-           if (err)
-               res.send(err);
-
-            res.json({ message: 'Bear created!' });
-        });
-      })
-
-     .get(function(req, res) {
+    .post(bearController.save(req, res))
+    .get(function(req, res) {
         Bear.find(function(err, bears) {
             if (err)
                 res.send(err);
