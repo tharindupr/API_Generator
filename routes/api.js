5c76a52db8b9d634f2f450d8f1035d24101dfbf0
Milindu Sanoj Kumarage <agentmilindu@gmail.com>
2015-08-25 11:49:42 +0530

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -10,7 +10,9 @@ var router = express.Router();              // get an instance of the express Ro
 router.route('/bears')
 
     // create a bear (accessed at POST http://localhost:8080/api/bears)
-    .post(bearController.save(req, res))
+    .post(function(req, res) {
+      bearController.save(req, res)
+    })
     .get(function(req, res) {
         Bear.find(function(err, bears) {
             if (err)
